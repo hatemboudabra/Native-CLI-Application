b3908c68de461fe2fbcb3119ad667c4edc462958
Hatem Boudabra <hatemboudabra41@gmail.com>
2024-08-12 10:45:13 +0200

    add graalvm

--- src/main/java/com/example/native_CLI_Application/NativeCliApplication.java
+++ src/main/java/com/example/native_CLI_Application/NativeCliApplication.java
@@ -2,7 +2,6 @@ package com.example.native_CLI_Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.command.annotation.EnableCommand;
 
 @SpringBootApplication
 public class NativeCliApplication {
